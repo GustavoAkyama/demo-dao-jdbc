326c77bba8a33dd93c13a202359c0553aa26e3c3
GustavoAkyama <gustavoakyamamga01@gmail.com>
2023-03-09 19:46:53 -0300

    Department entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,17 @@
 package application;
 
+import model.entities.Department;
+
+import java.util.Scanner;
+
 public class Program {
-    
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
+
+    }
 }
